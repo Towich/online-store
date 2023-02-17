bc91abbc1712f58a950ef10b9349c835486e38c3
Towich <towichf@gmail.com>
2023-02-17 19:43:18 +0300

    Add Toast to each method of life-cycle

--- app/src/main/java/com/example/firstpractice/MainActivity.java
+++ app/src/main/java/com/example/firstpractice/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.firstpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    int duration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+        duration = Toast.LENGTH_SHORT;
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast toast = Toast.makeText(context, "toast onStart", duration);
+        toast.show();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast toast = Toast.makeText(context, "toast onStop", duration);
+        toast.show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast toast = Toast.makeText(context, "toast onDestroy", duration);
+        toast.show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast toast = Toast.makeText(context, "toast onPause", duration);
+        toast.show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast toast = Toast.makeText(context, "toast onResume", duration);
+        toast.show();
     }
 }
