2fa5d64a57504fe8046952f201412e230b8fa5d3
Towich <towichf@gmail.com>
2023-02-17 20:01:25 +0300

    Add Logs to each life-cycle

--- app/src/main/java/com/example/firstpractice/MainActivity.java
+++ app/src/main/java/com/example/firstpractice/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyApplication";
+
 
     Context context;
     int duration;
@@ -18,30 +22,40 @@ public class MainActivity extends AppCompatActivity {
 
         context = getApplicationContext();
         duration = Toast.LENGTH_SHORT;
+
+        Log.d(TAG, "%Debug% in onCreate");
     }
     @Override
     protected void onStart() {
         super.onStart();
         Toast toast = Toast.makeText(context, "toast onStart", duration);
         toast.show();
+
+        Log.i(TAG, "%Info% in onStart");
     }
     @Override
     protected void onStop() {
         super.onStop();
         Toast toast = Toast.makeText(context, "toast onStop", duration);
         toast.show();
+
+        Log.w(TAG,"%Warning% in onStop");
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Toast toast = Toast.makeText(context, "toast onDestroy", duration);
         toast.show();
+
+        Log.e(TAG, "%ERROR% in onDestroy");
     }
     @Override
     protected void onPause() {
         super.onPause();
         Toast toast = Toast.makeText(context, "toast onPause", duration);
         toast.show();
+
+        Log.v(TAG, "%More info% in onPause");
     }
     @Override
     protected void onResume() {
