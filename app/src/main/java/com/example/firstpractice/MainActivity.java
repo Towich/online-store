269864dd4c91ea76b241b910e5a494fe7346fb8e
Towich <towichf@gmail.com>
2023-02-25 00:27:55 +0300

    added Activity Result API

--- app/src/main/java/com/example/firstpractice/MainActivity.java
+++ app/src/main/java/com/example/firstpractice/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.firstpractice;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.firstpractice.databinding.ActivityMainBinding;
@@ -21,6 +23,19 @@ public class MainActivity extends AppCompatActivity {
     Intent intent;
     Context context;
     int duration;
+    int totalPerfume = 0;
+
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Intent intent1 = result.getData();
+                    Bundle arguments = intent1.getExtras();
+                    totalPerfume = (int)arguments.get("total_perfume");
+                    Log.i(TAG, "Received from SecondActivity!");
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,39 +48,39 @@ public class MainActivity extends AppCompatActivity {
         intent = new Intent(this, SecondActivity.class);
 
 
-        Button button_lets_shop = binding.buttonLetsShop;
-        button_lets_shop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "opening secondActivity");
-                startActivity(intent);
-            }
+        ImageButton button_lets_shop = binding.buttonLetsShop;
+        button_lets_shop.setOnClickListener(view -> {
+            Log.i(TAG, "opening secondActivity");
+            intent.putExtra("total_perfume", totalPerfume);
+            mStartForResult.launch(intent);
+        });
+
+        binding.buttonAddPerfume.setOnClickListener(view -> {
+            totalPerfume++;
+            Toast.makeText(context, "Total perfume: " + totalPerfume, duration).show();
         });
     }
+
+
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(context, "toast onStart", duration).show();
     }
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(context, "toast onStop", duration).show();
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(context, "toast onDestroy", duration).show();
     }
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(context, "toast onPause", duration).show();
 
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(context, "toast onResume", duration).show();
     }
 }
--- app/src/main/java/com/example/firstpractice/SecondActivity.java
+++ app/src/main/java/com/example/firstpractice/SecondActivity.java
@@ -2,17 +2,16 @@ package com.example.firstpractice;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.firstpractice.databinding.ActivityMainBinding;
 import com.example.firstpractice.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
 
     ActivitySecondBinding binding;
     Intent intent;
+    int totalPerfume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +19,20 @@ public class SecondActivity extends AppCompatActivity {
 
         binding = ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         intent = new Intent(this, MainActivity.class);
 
-        binding.buttonExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(intent);
-            }
+        Bundle arguments = getIntent().getExtras();
+        totalPerfume = (int)arguments.get("total_perfume");
+        String newStr = "Total perfume: " + totalPerfume;
+
+        binding.totalPerfumeText.setText(newStr);
+
+
+        binding.buttonExit.setOnClickListener(view -> {
+            Intent intent1 = new Intent();
+            intent1.putExtra("total_perfume", totalPerfume);
+            setResult(1, intent1);
+            finish();
         });
     }
 
