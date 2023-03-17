c9e836a9493cabea735eea0708a5b434844ae9c1
Towich <towichf@gmail.com>
2023-03-18 01:36:15 +0300

    removed TOASTS

--- app/src/main/java/com/example/firstpractice/ContentFragment.java
+++ app/src/main/java/com/example/firstpractice/ContentFragment.java
@@ -29,8 +29,6 @@ public class ContentFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Toast.makeText(getContext(), "onCreateContentFragment", Toast.LENGTH_LONG).show();
         Log.i("Fragment", "onCreateContentFragment");
     }
 
@@ -38,7 +36,6 @@ public class ContentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_LONG).show();
         Log.i("Fragment", "onCreateView");
         // Inflate the layout for this fragment
 
@@ -58,7 +55,6 @@ public class ContentFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG).show();
         Log.i("Fragment", "onViewCreated");
 
 
@@ -97,71 +93,15 @@ public class ContentFragment extends Fragment {
         });
     }
 
-    @Override
-    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-        super.onViewStateRestored(savedInstanceState);
 
-        Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_LONG).show();
-        Log.i("Fragment", "onViewStateRestored");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        Toast.makeText(getContext(), "onStart", Toast.LENGTH_LONG).show();
-        Log.i("Fragment", "onStart");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        Toast.makeText(getContext(), "onResume", Toast.LENGTH_LONG).show();
-        Log.i("Fragment", "onResume");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        Toast.makeText(getContext(), "onPause", Toast.LENGTH_LONG).show();
-        Log.i("Fragment", "onPause");
-    }
 
     @Override
     public void onStop() {
         super.onStop();
-
-        Toast.makeText(getContext(), "onStop", Toast.LENGTH_LONG).show();
-        Log.i("Fragment", "onStop");
 
         Bundle result = new Bundle();
         result.putInt("perfumeCounter", perfumeCounter);
         fragmentManager.setFragmentResult("requestKey", result);
     }
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_LONG).show();
-        Log.i("Fragment", "onSaveInstanceState");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-
-        Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_LONG).show();
-        Log.i("Fragment", "onDestroyView");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        Toast.makeText(getContext(), "onDestroy", Toast.LENGTH_LONG).show();
-        Log.i("Fragment", "onDestroy");
-    }
 }
