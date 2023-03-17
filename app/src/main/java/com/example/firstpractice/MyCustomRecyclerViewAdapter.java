0fa5630b1e259d37be0a8a2f5fe2c4ea20962293
Towich <towichf@gmail.com>
2023-03-18 01:08:31 +0300

    realized RecycleView in StoreDiscountsFragment

---
+++ app/src/main/java/com/example/firstpractice/MyCustomRecyclerViewAdapter.java
@@ -0,0 +1,65 @@
+package com.example.firstpractice;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyCustomRecyclerViewAdapter extends RecyclerView.Adapter<MyCustomRecyclerViewAdapter.ViewHolder> {
+    private LayoutInflater inflater;
+    private List<String> store_items;
+    private Context context;
+
+    public MyCustomRecyclerViewAdapter(Context context, List<String> items){
+        store_items = items;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getItemCount() {
+        return store_items.size();
+    }
+
+    @NonNull
+    @Override
+    public MyCustomRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.store_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyCustomRecyclerViewAdapter.ViewHolder holder, int position) {
+        holder.store_item_text.setText(store_items.get(position));
+        holder.store_item_image.setImageResource(R.drawable._551570);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, store_items.get(holder.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                Log.i("RecyclerView", store_items.get(holder.getAdapterPosition()));
+            }
+        });
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView store_item_text;
+        ImageView store_item_image;
+
+        public ViewHolder(View view){
+            super(view);
+            store_item_text = view.findViewById(R.id.store_item_text);
+            store_item_image = view.findViewById(R.id.store_item_image);
+        }
+    }
+}
+
+
--- app/src/main/java/com/example/firstpractice/StoreDiscountsFragment.java
+++ app/src/main/java/com/example/firstpractice/StoreDiscountsFragment.java
@@ -1,10 +1,20 @@
 package com.example.firstpractice;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class StoreDiscountsFragment extends Fragment {
@@ -20,5 +30,23 @@ public class StoreDiscountsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_store_discounts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        List<String> store_items = new ArrayList<String>();
+
+        for(int i = 1; i <= 300; i++){
+            store_items.add("Premium perfume " + i);
+        }
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 1);
+        MyCustomRecyclerViewAdapter recyclerViewAdapter = new MyCustomRecyclerViewAdapter(getActivity(), store_items);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
 }
