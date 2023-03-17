97b154f10238ac33653870111ace14055a70bb9a
Towich <towichf@gmail.com>
2023-03-17 23:45:28 +0300

    realized ListView in StoreFragment

--- app/src/main/java/com/example/firstpractice/StoreFragment.java
+++ app/src/main/java/com/example/firstpractice/StoreFragment.java
@@ -1,10 +1,17 @@
 package com.example.firstpractice;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class StoreFragment extends Fragment {
@@ -19,5 +26,21 @@ public class StoreFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_store, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        List<String> store_items = new ArrayList<String>();
+        for (int i = 1; i < 300; i++){
+            store_items.add("Perfume " + i);
+        }
+
+        MyCustomListViewAdapter listViewAdapter = new MyCustomListViewAdapter(
+                getActivity(),R.layout.store_item, store_items);
+
+        ListView listView = view.findViewById(R.id.list_view);
+        listView.setAdapter(listViewAdapter);
     }
 }
