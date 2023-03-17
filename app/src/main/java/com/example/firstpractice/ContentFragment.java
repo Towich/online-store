38151c78889c5d3de0d396b727a1a18f024b732e
Towich <towichf@gmail.com>
2023-03-17 18:28:20 +0300

    added Toasts and Logs

--- app/src/main/java/com/example/firstpractice/ContentFragment.java
+++ app/src/main/java/com/example/firstpractice/ContentFragment.java
@@ -27,11 +27,16 @@ public class ContentFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Toast.makeText(getContext(), "onCreateContentFragment", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onCreateContentFragment");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onCreateView");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
@@ -39,6 +44,8 @@ public class ContentFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onViewCreated");
 
         ImageButton btn_cart = view.findViewById(R.id.button_lets_shop);
         Button btnAddToCart = view.findViewById(R.id.button_add_perfume);
@@ -61,5 +68,69 @@ public class ContentFragment extends Fragment {
                 Log.i("Fragment", "Total perfume: " + perfumeCounter);
             }
         });
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+
+        Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onViewStateRestored");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Toast.makeText(getContext(), "onStart", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Toast.makeText(getContext(), "onResume", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Toast.makeText(getContext(), "onPause", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Toast.makeText(getContext(), "onStop", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onStop");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onSaveInstanceState");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(getContext(), "onDestroy", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onDestroy");
     }
 }
